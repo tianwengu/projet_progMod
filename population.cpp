285f7413feb9addc26ade575c978fefda9628114
hunter-zhang@outlook.com <hunter-zhang@outlook.com>
2022-04-30 12:28:36 +0200

    添加population.cpp

---
+++ population.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<array>
+#include<cstdlib>
+#include<stdexcept>
+#include<algorithm>
+#include "systeme.hpp"
+using namespace std;
+
+Population();
+        Animal get(int id)const;
+        vector<int> getIds()const;
+        int reserve();
+        void set(Animal a);
+        void supprime(int id);
