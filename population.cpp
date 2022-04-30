769b339e803e75e00b2484562c0f7081878257e8
hunter-zhang@outlook.com <hunter-zhang@outlook.com>
2022-04-30 12:32:01 +0200

    population

--- population.cpp
+++ population.cpp
@@ -6,8 +6,15 @@
 #include "systeme.hpp"
 using namespace std;
 
-Population();
-        Animal get(int id)const;
+Animal Population::get(int id)const{
+    for(int i = 0; i < animaux.size();i++){
+        if(animaux[i].getId()==id)
+            return animaux[i];
+    }
+    throw invalid_argument("Pas de animal avec cet id !");
+}
+     
+     
         vector<int> getIds()const;
         int reserve();
         void set(Animal a);
