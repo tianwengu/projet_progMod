893a977c81fae645afc138d5f82f893e829c4767
hunter-zhang@outlook.com <hunter-zhang@outlook.com>
2022-05-04 14:40:24 +0200

    part

--- population.cpp
+++ population.cpp
@@ -3,8 +3,16 @@
 #include<cstdlib>
 #include<stdexcept>
 #include<algorithm>
+#include<vector>
+
 #include "systeme.hpp"
 using namespace std;
+
+Population::Population(){
+    for(int i = 0; i < TAILLEGRILLE*TAILLEGRILLE;i++){
+        caseLibre.push_back(Coord(i));
+    }
+}
 
 Animal Population::get(int id)const{
     for(int i = 0; i < animaux.size();i++){
@@ -26,5 +34,20 @@ vector<int> Population::getIds()const{
 int Population::reserve(){
     
 }
-        void set(Animal a);
-        void supprime(int id);
+void Population::set(Animal a){
+    animaux.push_back(a);
+
+}
+        
+void Population::supprime(int id){
+    for(int i = 0; i < animaux.size();i++){
+        if(animaux[i].getId()==id){
+            for(int j = 0; j < caseLibre.size();j++){
+                if(caseLibre[j] == animaux[i].getCoord())
+                    caseLibre.erase(j);
+            }
+            animaux.erase(i);
+        }
+    }
+    for(int i = 0; i < caseLibre.size();i++)
+}
